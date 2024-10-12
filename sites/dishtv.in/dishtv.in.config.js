dc9408b54efb7cf5680839b65ede0e5961b0e8a2
whythishome <84956229+whythishome@users.noreply.github.com>
2024-10-12 17:02:10 +0530

    Update dishtv.in.config.js

--- sites/dishtv.in/dishtv.in.config.js
+++ sites/dishtv.in/dishtv.in.config.js
@@ -39,7 +39,7 @@ module.exports = {
       programs.push({
         title: item.title,
         description: item.desc,
-        image: item.image.landscape,
+        image: item.image,
         start: parseStart(item),
         stop: parseStop(item)
       })
