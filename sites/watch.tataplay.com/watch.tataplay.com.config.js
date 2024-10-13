4d8d58f8eadcceeb1dd3bd54b1dccace2f0b9808
whythishome <84956229+whythishome@users.noreply.github.com>
2024-10-13 15:59:35 +0530

    Create watch.tataplay.com.config.js

---
+++ sites/watch.tataplay.com/watch.tataplay.com.config.js
@@ -0,0 +1,163 @@
+const axios = require('axios')
+const dayjs = require('dayjs')
+const utc = require('dayjs/plugin/utc')
+const timezone = require('dayjs/plugin/timezone')
+const customParseFormat = require('dayjs/plugin/customParseFormat')
+
+let TOKEN
+
+dayjs.extend(utc)
+dayjs.extend(timezone)
+dayjs.extend(customParseFormat)
+
+module.exports = {
+  site: 'dishtv.in',
+  days: 1,
+  url({ date, channel }) {
+    const [region] = channel.site_id.split('#')
+
+    return `https://tm.tapi.videoready.tv/portal-search/pub/api/v1/channels/schedule${region}_${date.format('YYYY-MM-DD')}.json`
+  },
+  request: {
+    method: 'GET',
+    headers: function() {
+      return setHeaders()
+    },
+    data({
+      channel,
+      date
+    }) {
+      return {
+        allowPastEvents: true,
+        channelid: channel.site_id,
+        date: date.format('DD/MM/YYYY')
+      }
+    }
+  },
+  parser: function({
+    content
+  }) {
+    let programs = []
+    const items = parseItems(content)
+    items.forEach(item => {
+      programs.push({
+        title: item.title,
+        description: item.desc,
+        image: item.programmeurl,
+        start: parseStart(item),
+        stop: parseStop(item)
+      })
+    })
+    return programs
+  }
+  // async channels() {
+  //   let channels = []
+  //   const url = 'https://www.dishtv.in/services/epg/channels'
+  //   const params = {
+  //     headers: await setHeaders()
+  //   }
+  //   const pages = await fetchPages()
+
+  //   for (let i = 0; i < Number(pages); i++) {
+  //     const body = {
+  //       pageNum: i + 1
+  //     }
+  //     const data = await axios
+  //       .post(url, body, params)
+  //       .then(r => r.data)
+  //       .catch(console.log)
+
+  //     data.programDetailsByChannel.forEach(channel => {
+  //       if (channel.channelname === '.') return
+  //       channels.push({
+  //         lang: 'en',
+  //         site_id: channel.channelid,
+  //         name: channel.channelname
+  //       })
+  //     })
+  //   }
+
+  //   return channels
+  // }
+}
+
+function parseStart(item) {
+  return dayjs.tz(item.start, 'YYYY-MM-DDTHH:mm:ss', 'Asia/Kolkata')
+}
+
+function parseStop(item) {
+  return dayjs.tz(item.stop, 'YYYY-MM-DDTHH:mm:ss', 'Asia/Kolkata')
+}
+
+function parseItems(content) {
+  const data = JSON.parse(content)
+  if (!data || !Array.isArray(data)) return []
+
+  return data
+}
+
+async function fetchPages() {
+  const url = 'https://www.dishtv.in/services/epg/channels'
+  const body = {
+    pageNum: 1,
+  }
+  const params = {
+    headers: await setHeaders()
+  }
+  const data = await axios
+    .post(url, body, params)
+    .then(r => r.data)
+    .catch(console.log)
+
+  return data.pageNum
+}
+
+// Function to try to fetch TOKEN
+function fetchToken() {
+  return fetch(
+      'https://www.dishtv.in/services/epg/signin', {
+        headers: {
+          accept: 'application/json, text/javascript, */*; q=0.01',
+          'sec-fetch-dest': 'empty',
+          'sec-fetch-mode': 'cors',
+          'sec-fetch-site': 'same-origin',
+          'x-requested-with': 'XMLHttpRequest',
+          Referer: 'https://www.dishtv.in/channel-guide.html'
+        },
+        method: 'POST'
+      }
+    )
+    .then(response => {
+      // Check if the response status is OK (2xx)
+      if (!response.ok) {
+        throw new Error('HTTP request failed')
+      }
+      return response.json()
+    })
+    .then(data => {
+      if (data.token) {
+        TOKEN = data.token
+      } else {
+        console.log('TOKEN not found in the response.')
+      }
+    })
+    .catch(error => {
+      console.error(error)
+    })
+}
+
+function setHeaders() {
+  if (!TOKEN) {
+    return fetchToken().then(() => {
+      return {
+        'Content-Type': 'application/json',
+        'Authorization': TOKEN
+      }
+    })
+  } else {
+    return {
+      'Content-Type': 'application/json',
+      'Authorization': TOKEN
+    }
+  }
+}
