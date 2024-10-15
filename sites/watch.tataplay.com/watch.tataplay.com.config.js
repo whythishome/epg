23ca0bed24f38457c799739536218b04ac1e4662
whythishome <84956229+whythishome@users.noreply.github.com>
2024-10-15 19:42:53 +0530

    Update watch.tataplay.com.config.js

--- sites/watch.tataplay.com/watch.tataplay.com.config.js
+++ sites/watch.tataplay.com/watch.tataplay.com.config.js
@@ -11,9 +11,9 @@ dayjs.extend(timezone)
 dayjs.extend(customParseFormat)
 
 module.exports = {
-  site: 'dishtv.in',
+  site: 'watch.tataplay.com',
   days: 1,
-  url({ date }) {
+  url: function ({ date }) {
     return `https://tm.tapi.videoready.tv/content-detail/pub/api/v2/channels/schedule?date=${date.format('DD-MM-YYYY')}`
   },
   request({ channel }) {
