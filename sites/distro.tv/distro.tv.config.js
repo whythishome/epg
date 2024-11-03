c29cc28b997e6fecc247bbb4959ac9e86168f7d3
whythishome <84956229+whythishome@users.noreply.github.com>
2024-11-03 18:35:26 +0530

    Update distro.tv.config.js

--- sites/distro.tv/distro.tv.config.js
+++ sites/distro.tv/distro.tv.config.js
@@ -1,18 +1,16 @@
 const axios = require('axios')
 const dayjs = require('dayjs')
 
-const APP_KEY = '5ee2ef931de1c4001b2e7fa3_5ee2ec25a0e845001c1783dc'
-const SESSION_KEY = '01G2QG0N3RWDNCBA1S5MK1MD2K17CE4431A2'
-
 module.exports = {
   site: 'distro.tv',
   days: 1,
   url: function ({ date, channel }) {
-    return `https://waf-starhub-metadata-api-p001.ifs.vubiquity.com/v3.1/epg/schedules?locale=en_US&locale_default=en_US&device=1&in_channel_id=${channel.site_id}&gt_end=${date.unix()}&lt_start=${date.add(1, 'd').unix()}&limit=100&page=1`
+    return `https://tv.jsrdn.com/epg/query.php?range=now,24h&id=${channel.site_id}`
   },
   parser: function ({ content, channel }) {
     let programs = []
-    const items = parseItems(content, channel)
+    const epg = parseItems(content, channel)
+    const items = content.epg[channel.site_id].slots
     items.forEach(item => {
       programs.push({
         title: item.title,
@@ -21,7 +19,6 @@ module.exports = {
         stop: parseStop(item)
       })
     })
-
     return programs
   },  
   async channels() {
@@ -45,14 +42,14 @@ module.exports = {
 }
 
 function parseStart(item) {
-  return dayjs.unix(item.start)
+  return dayjs(item.start)
 }
 
 function parseStop(item) {
-  return dayjs.unix(item.end)
+  return dayjs(item.end)
 }
 
 function parseItems(content, channel) {
   const data = JSON.parse(content)
-  return data.resources ? data.resources : []
+  return data ? data : []
 }
