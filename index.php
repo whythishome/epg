f3c954e30245b91758e8afa5d5f927711ab7efda
whythishome <84956229+whythishome@users.noreply.github.com>
2024-11-05 18:22:05 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 $get = $_GET['get'];
-$url = $get;
+$url = 'https://jiotvapi.cdn.jio.com/apis/v3.0/getMobileChannelList/get/?langid=6&devicetype=phone&os=android&usertype=JIO&version=34';
 
 $heads = [
   'http' => [
