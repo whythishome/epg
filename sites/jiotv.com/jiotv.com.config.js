e6bdf4cf869075d8a669fc8f964ca60c7a9ecaca
whythishome <84956229+whythishome@users.noreply.github.com>
2024-11-05 23:39:33 +0530

    Update jiotv.com.config.js

--- sites/jiotv.com/jiotv.com.config.js
+++ sites/jiotv.com/jiotv.com.config.js
@@ -72,6 +72,13 @@ function parseStop(item) {
 }
 
 function parseItems(content, channel) {
-  const data = content
+  const chunksParser = body => {
+    return body
+      .replace(/^(\w{1,3})\r\n/, "") // replace header chunks info 
+      .replace(/\r\n(\w{1,3})\r\n/, "") // replace in-body chunks info
+      .replace(/(\r\n0\r\n\r\n)$/, ""); // replace end chunks info
+    };
+  const body = chunksParser(content)
+  const data = JSON.parse(body)
   return data.epg ? data.epg : []
 }
