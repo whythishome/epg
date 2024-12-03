31cedfa67b5ebe334478e01c98d6c73d45f129c8
whythishome <84956229+whythishome@users.noreply.github.com>
2024-12-03 16:00:34 +0530

    Update foxtel.com.au.config.js

--- sites/foxtel.com.au/foxtel.com.au.config.js
+++ sites/foxtel.com.au/foxtel.com.au.config.js
@@ -4,7 +4,7 @@ const cheerio = require('cheerio')
 
 module.exports = {
   site: 'foxtel.com.au',
-  days: 2,
+  days: 7,
   url({ channel, date }) {
     return `https://www.foxtel.com.au/tv-guide/channel/${channel.site_id}/${date.format(
       'YYYY/MM/DD'
