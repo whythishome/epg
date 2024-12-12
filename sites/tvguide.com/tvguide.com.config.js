e4f29d3be4081c3161865c413a91e56d3195cdf0
whythishome <84956229+whythishome@users.noreply.github.com>
2024-12-12 11:32:53 +0530

    Update tvguide.com.config.js

--- sites/tvguide.com/tvguide.com.config.js
+++ sites/tvguide.com/tvguide.com.config.js
@@ -55,7 +55,7 @@ module.exports = {
           lang: 'en',
           site_id: `${providerId}#${item.sourceId}`,
           name: item.fullName,
-          icon: item.logo ? `https://www.tvguide.com/a/img/catalog` + {item.logo} : null
+          icon: item.['logo'] ? `https://www.tvguide.com/a/img/catalog` + item['logo'] : null
         })
       })
     }
