18bd1834ee7955eaa37f9464deea2f7be7eb80c2
whythishome <84956229+whythishome@users.noreply.github.com>
2024-12-12 18:35:46 +0530

    Update tvtv.us.config.js

--- sites/tvtv.us/tvtv.us.config.js
+++ sites/tvtv.us/tvtv.us.config.js
@@ -35,8 +35,18 @@ module.exports = {
     const data = await axios
       .get( `https://tvtv.us/api/v1/lineup/USA-ME18440-X/channels`, {
         headers : {
-          'Referer': 'https://tvtv.us/me/portland/04102/luUSA-ME18440-X',
-          'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36'
+          'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+          'accept-language': 'en-US,en;q=0.9',
+          'priority': 'u=0, i',
+          'sec-ch-ua': '"Google Chrome";v="131", "Chromium";v="131", "Not_A Brand";v="24"',
+          'sec-ch-ua-mobile': '?0',
+          'sec-ch-ua-platform': '"Windows"',
+          'sec-fetch-dest': 'document',
+          'sec-fetch-mode': 'navigate',
+          'sec-fetch-site': 'none',
+          'sec-fetch-user': '?1',
+          'upgrade-insecure-requests': '1',
+          'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36'
         }
       }).then(r => r.data)
       .catch(console.log)
