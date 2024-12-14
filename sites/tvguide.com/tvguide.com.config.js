1f4a51c7dd46aef518b73093c562671d68296627
whythishome <84956229+whythishome@users.noreply.github.com>
2024-12-14 23:10:05 +0530

    Update tvguide.com.config.js

--- sites/tvguide.com/tvguide.com.config.js
+++ sites/tvguide.com/tvguide.com.config.js
@@ -1,22 +1,22 @@
-const axios = require('axios')
-const dayjs = require('dayjs')
-const utc = require('dayjs/plugin/utc')
-const timezone = require('dayjs/plugin/timezone')
+const axios = require('axios');
+const dayjs = require('dayjs');
+const utc = require('dayjs/plugin/utc');
+const timezone = require('dayjs/plugin/timezone');
 
-dayjs.extend(utc)
-dayjs.extend(timezone)
+dayjs.extend(utc);
+dayjs.extend(timezone);
 
 module.exports = {
   site: 'tvguide.com',
   delay: 2500,
   days: 1,
   url: function ({ date, channel }) {
-    const [providerId, channelSourceIds] = channel.site_id.split('#')
+    const [providerId, channelSourceIds] = channel.site_id.split('#');
     const url = `https://backend.tvguide.com/tvschedules/tvguide/${providerId}/web?start=${date
       .startOf('d')
-      .unix()}&duration=12000&channelSourceIds=${channelSourceIds}&apiKey=DI9elXhZ3bU6ujsA2gXEKOANyncXGUGc`
+      .unix()}&duration=12000&channelSourceIds=${channelSourceIds}&apiKey=DI9elXhZ3bU6ujsA2gXEKOANyncXGUGc`;
 
-    return url
+    return url;
   },
   request: {
     method: 'GET',
@@ -29,27 +29,27 @@ module.exports = {
     const programs = [];
     const items = parseItems(content);
     for (let item of items) {
-      const details = await loadProgramDetails(item)
+      const details = await loadProgramDetails(item);
       programs.push({
         title: item.title,
         description: details.description,
         start: parseTime(item.startTime),
         stop: parseTime(item.endTime)
-      })
+      });
     }
-    return programs
+    return programs;
   },
   async channels() {
-    const providers = [9133001044]
+    const providers = [9133001044];
 
-    let channels = []
+    let channels = [];
     for (let providerId of providers) {
       const data = await axios
         .get(
           `https://backend.tvguide.com/tvschedules/tvguide/serviceprovider/${providerId}/sources/web?apiKey=DI9elXhZ3bU6ujsA2gXEKOANyncXGUGc`
         )
         .then(r => r.data)
-        .catch(console.log)
+        .catch(console.log);
 
       data.data.items.forEach(item => {
         channels.push({
@@ -57,43 +57,43 @@ module.exports = {
           site_id: `${providerId}#${item.sourceId}`,
           name: item.fullName,
           logo: item.logo ? `https://www.tvguide.com/a/img/catalog${item.logo}` : null
-        })
-      })
+        });
+      });
     }
 
-    return channels
+    return channels;
   }
-}
+};
 
 function parseRating(item) {
-  return item.rating ? { system: 'MPA', value: item.rating } : null
+  return item.rating ? { system: 'MPA', value: item.rating } : null;
 }
 
 function parseCategories(details) {
-  return Array.isArray(details.genres) ? details.genres.map(g => g.name) : []
+  return Array.isArray(details.genres) ? details.genres.map(g => g.name) : [];
 }
 
 function parseTime(timestamp) {
-  return dayjs.unix(timestamp)
+  return dayjs.unix(timestamp);
 }
 
 function parseItems(content) {
-  const data = JSON.parse(content)
-  if (!data.data || !Array.isArray(data.data.items) || !data.data.items.length) return []
+  const data = JSON.parse(content);
+  if (!data.data || !Array.isArray(data.data.items) || !data.data.items.length) return [];
 
-  return data.data.items[0].programSchedules
+  return data.data.items[0].programSchedules;
 }
 
 async function loadProgramDetails(item) {
   const programDetailsUrl = `${item.programDetails}?apiKey=DI9elXhZ3bU6ujsA2gXEKOANyncXGUGc`;
   const data = await makeRequest(programDetailsUrl, { headers: setHeaders() });
-  if (!data || !data.data || !data.data.item) return {}
+  if (!data || !data.data || !data.data.item) return {};
 
-  return data.data.item
+  return data.data.item;
 }
 
-function setHeaders() {
-  return {
+function setHeaders(cookies = '') {
+  const headers = {
     'accept': 'application/json, text/plain, */*',
     'accept-language': 'en-US,en;q=0.9',
     'origin': 'https://www.tvguide.com',
@@ -106,17 +106,28 @@ function setHeaders() {
     'sec-fetch-mode': 'cors',
     'sec-fetch-site': 'same-site',
     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36'
+  };
+
+  if (cookies) {
+    headers['cookie'] = cookies;
   }
+
+  return headers;
 }
+
+let cookies = '';
 
 async function makeRequest(url, options) {
   try {
     const response = await axios.get(url, options);
+    if (response.headers['set-cookie']) {
+      cookies = response.headers['set-cookie'].join('; ');
+    }
     return response.data;
   } catch (error) {
     if (error.response && error.response.status === 403) {
       console.log('Received 403 error, making a call to https://www.tvguide.com/listings/');
-      await axios.get('https://www.tvguide.com/listings/', { headers: setHeaders() });
+      await axios.get('https://www.tvguide.com/listings/', { headers: setHeaders(cookies) });
       console.log('Retrying the original URL');
       return makeRequest(url, options); // Retry the original request
     } else {
