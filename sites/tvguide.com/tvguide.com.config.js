33d9d240d886816e04bb860aca8c8eb73de75250
whythishome <84956229+whythishome@users.noreply.github.com>
2024-12-15 20:57:47 +0530

    Update tvguide.com.config.js

--- sites/tvguide.com/tvguide.com.config.js
+++ sites/tvguide.com/tvguide.com.config.js
@@ -59,18 +59,18 @@ module.exports = {
       let newDescription = '';
       if (episodeTitle) newDescription += ` ${episodeTitle}.`;
       if (seasonNumber && episodeNumber) newDescription += ` S${seasonNumber}E${episodeNumber}.`;
-      newDescription += ` ${description}.`;
+      newDescription += ` ${description}`;
       if (formattedDate) newDescription += ` (${formattedDate}).`;
       if (tvRating) newDescription += ` ${tvRating}.`;
       if (firstGenre) newDescription += ` ${firstGenre}`;
-      if (secondGenre) newDescription += ` / ${secondGenre}`;
+      if (secondGenre) newDescription += ` / ${secondGenre}.`;
 
       if (details.type == 'movie') {
         newDescription = description;
         if (releaseYear) newDescription += ` (${releaseYear}).`;
         if (tvRating) newDescription += ` ${tvRating}.`;
         if (firstGenre) newDescription += ` ${firstGenre}`;
-        if (secondGenre) newDescription += ` / ${secondGenre}`;
+        if (secondGenre) newDescription += ` / ${secondGenre}.`;
       }
     
       programs.push({
