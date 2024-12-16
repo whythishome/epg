8cd922f456a7fd1a036d99fb69200d278c9e8399
whythishome <84956229+whythishome@users.noreply.github.com>
2024-12-16 18:45:01 +0530

    Update tvguide.com.config.js

--- sites/tvguide.com/tvguide.com.config.js
+++ sites/tvguide.com/tvguide.com.config.js
@@ -12,7 +12,7 @@ let useProxy = false; // Toggle flag to alternate requests per channel
 
 module.exports = {
   site: 'tvguide.com',
-  delay: 3000,
+  delay: 4000,
   days: 1,
   url: function ({ date, channel }) {
     const [providerId, channelSourceIds] = channel.site_id.split('#');
