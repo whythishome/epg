99628e480a193faa5a479dd8d1b8c7666d6c4796
whythishome <84956229+whythishome@users.noreply.github.com>
2025-07-27 22:02:56 +0530

    Update web.runn.tv.config.js

--- sites/web.runn.tv/web.runn.tv.config.js
+++ sites/web.runn.tv/web.runn.tv.config.js
@@ -61,6 +61,7 @@ function parseItems(content) {
 }
 function setHeaders() {
   return {
+    'Host': 'prod-epg.runn.tv',
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36',
     'content-type': 'application/json'
   };
