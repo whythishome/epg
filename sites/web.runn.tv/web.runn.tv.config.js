694caac30dbfdd657e2e17ef56c6e625be80d8a1
whythishome <84956229+whythishome@users.noreply.github.com>
2025-07-27 21:38:40 +0530

    Update web.runn.tv.config.js

--- sites/web.runn.tv/web.runn.tv.config.js
+++ sites/web.runn.tv/web.runn.tv.config.js
@@ -9,14 +9,14 @@ let tokenPromise = null
 
 module.exports = {
   site: 'web.runn.tv',
-  days: 2,
+  days: 1,
   url({ channel }) {
     return `https://prod-epg.runn.tv/runtv/v1/schedule/getChannelEpg/${channel.site_id}`
   },
   request: {
     method: 'GET',
-    headers: function() {
-      return getHeaders();
+    headers: {
+      userid: '0D-62-2D-15-FD-CE'
     }
   },
   parser: ({ channel, content }) => {
@@ -37,35 +37,6 @@ module.exports = {
     })
 
     return programs
-  },
-  async channels({ lang }) {
-    const totalPages = await fetchPages('https://www.dishtv.in/services/epg/channels')
-
-    const channels = []
-    for (let i = 0; i < Number(totalPages); i++) {
-      const body = new FormData()
-      body.append('pageNum', i + 1)
-
-      try {
-        const resp = await axios.post('https://www.dishtv.in/services/epg/channels', body, {
-          headers: { 'authorization-token': (await getHeaders()).Authorization }
-        })
-        resp.data.programDetailsByChannel.forEach(channel => {
-          if (channel.channelname !== '.') {
-            channels.push({
-              lang,
-              site_id: channel.channelid,
-              name: channel.channelname,
-              logo: channel.channelimage
-            })
-          }
-        })
-      } catch (error) {
-        console.error(`Error fetching channels on page ${i + 1}:`, error.message)
-      }
-    }
-
-    return channels
   }
 }
 
